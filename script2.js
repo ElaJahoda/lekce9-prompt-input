1490ecb897c3cfbf0dd13247f21d5888e5355481
Ela Jahoda <3la.jah0da@gmail.com>
2022-04-15 15:15:26 +0200

    funkcionalita a implementace zmeny matematickeho znamenka

--- script2.js
+++ script2.js
@@ -4,19 +4,39 @@
 // - Interně můžeš použít funkci z Lekce 1: https://github.com/Czechitas-JavaScript-1/lekce1-funkce-udalosti/blob/main/main.js
 // - Minibonus: Pokud chceš, můžeš rozšířit na odečítání, násobení i dělení (jak budeš mezi operacemi přepínat?).
 
-let vysledek = document.querySelector('#vysledek > span');
 let tlacitko = document.querySelector('button');
 
-function scitani(cislo1, cislo2) {
-    let c = cislo1 + cislo2;
+function scitani(cislo1, cislo2, znamenko) {
+    let c = 0;
+    if(znamenko === '+') {
+        c = cislo1 + cislo2;
+    } else if(znamenko === '-') {
+        c = cislo1 - cislo2;
+    } else if(znamenko === '/' && znamenko !== 0) {
+        c = cislo1 / cislo2;
+    } else if(znamenko === '*') {
+        c = cislo1 * cislo2;
+    }
     return c;
 }
 
 tlacitko.addEventListener('click',() => {
+let vysledek = document.querySelector('#vysledek > span');
 let cislo1 = parseInt(document.querySelector('input[name=cislo1]').value);
 let cislo2 = parseInt(document.querySelector('input[name=cislo2]').value);
-console.log(cislo1);
-console.log(cislo2);
-console.log(scitani(cislo1, cislo2));
-vysledek.innerHTML = scitani(cislo1, cislo2);
-})
+let znamenko = vybraneZnamenko();
+if(znamenko != null && cislo1 != NaN && cislo2 != NaN) {
+vysledek.innerHTML = scitani(cislo1, cislo2, znamenko);
+}else{
+    vysledek.innerHTML = 'Neni zadan cely priklad. Doplnte znak pro matematickou operaci, jedno nebo druhe cislo a opakujte akci.';
+}} )
+
+function vybraneZnamenko() { 
+    let znamenko = document.querySelector('input[name="znamenko"]:checked');
+    if(znamenko != null) {
+        return(znamenko.value);
+    }
+    else {
+        return null;
+    }
+}
